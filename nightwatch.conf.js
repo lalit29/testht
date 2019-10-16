8dcd17a1ecb03d6899ee16e4f5f08282629951ce
lalit <lalit@hometown.in>
2019-10-16 16:17:05 +0530

    test

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -40,16 +40,16 @@ module.exports={
     //
     },
 
-  //   "chrome" : {
-  //     "desiredCapabilities": {
-  //       "browserName": "chrome"
-  //     },
-  //     'chromeOptions': {
-  //      w3c: false,
-  //      'args': ['--headless', "--window-size=1860,1200"],
-  //   }
+    "chrome" : {
+      "desiredCapabilities": {
+        "browserName": "chrome"
+      },
+      'chromeOptions': {
+       w3c: false,
+       'args': ['--headless', "--window-size=1860,1200"],
+    }
       
-  // },
+  },
 
   chrome : {
 "desiredCapabilities": {
@@ -76,7 +76,8 @@ w3c: false,
 ],
 },
 }
+}
   
-   }
+   
 }
 }
