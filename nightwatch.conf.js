22c2a354f1a66f593362a546e2b21a74aba2b49d
lalit <lalit@hometown.in>
2019-10-18 15:26:53 +0530

    test

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,9 +1,11 @@
+//const chrome = require('chromedriver')
 module.exports={
     "src_folders" : ["tests"],
 
   "webdriver" : {
     "start_process": true,
     "server_path": "node_modules/.bin/chromedriver",
+    //"server_path": chrome.path,
     //"server_path": "./bin/geckodriver-0.23",//
     "port": 9515
   },
@@ -28,7 +30,8 @@ module.exports={
           'enabled': true,
           'on_failure': true,
           'on_error': true,
-          'path': 'tests_output/screenshots'
+          'path': 'tests_output/screenshots',
+          //'path':__dirname + 'tests_output/screenshots',
       },
     //   "desiredCapabilities" : {
     //     "browserName" : "phantomjs",
